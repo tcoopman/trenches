8e41b2ea64474df3fac7a04425a0298b6a162fd2
Thomas Coopman <thomas.coopman@gmail.com>
2017-05-03 21:38:02 +0200

    broadcast tick

--- lib/trenches/game.ex
+++ lib/trenches/game.ex
@@ -7,7 +7,13 @@ defmodule Trenches.Game do
   alias Trenches.Player
 
   def start_link() do
-    GenServer.start_link(__MODULE__, %{unique_id: 0, players: []}, name: :game)
+    GenServer.start_link(__MODULE__, %{}, name: :game)
+  end
+
+  def init(state) do
+    state = %{unique_id: 0, players: [], subscribers: MapSet.new}
+    schedule_work()
+    {:ok, state}
   end
 
   def join() do
@@ -18,14 +24,36 @@ defmodule Trenches.Game do
 
   end
 
+  def subscribe(subscriber) do
+    GenServer.call(:game, {:subscribe, subscriber})
+  end
+
   # server
 
   def handle_call(:join, _from, %{unique_id: id, players: players} = state) do
     if id < 2 do
       player = %Player{id: id}
     end
-    state = %{unique_id: (id+1), players: ([player | players])}
+    state = %{state | unique_id: (id+1), players: ([player | players])}
 
     {:reply, id, state}
+  end
+
+  def handle_call({:subscribe, subscriber}, _from, %{subscribers: subscribers} = state) do
+    subscribers = MapSet.put(subscribers, subscriber)
+    state = %{state | subscribers: subscribers}
+    {:reply, state, state}
+  end
+
+  def handle_info(:tick, %{subscribers: subscribers, players: players} = state) do
+    Enum.each(subscribers, fn subscriber -> 
+      send(subscriber, {:tick, players})
+    end)
+    schedule_work()
+    {:noreply, state}
+  end
+
+  defp schedule_work() do
+    Process.send_after(self(), :tick, 500) # In 2 hours
   end
 end
