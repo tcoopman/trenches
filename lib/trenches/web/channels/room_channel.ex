b55efe6b20edab213bf727b2de568281c3d9bf8d
Thomas Coopman <thomas.coopman@gmail.com>
2017-05-03 21:18:23 +0200

    join player

--- lib/trenches/web/channels/room_channel.ex
+++ lib/trenches/web/channels/room_channel.ex
@@ -1,7 +1,18 @@
 defmodule Trenches.Web.RoomChannel do
   use Phoenix.Channel
 
+  alias Trenches.Game
+
   def join("room:game", _message, socket) do
+    id = Game.join()
+    send(self, {:after_join, id})
     {:ok, socket}
   end
+
+  def handle_info({:after_join, id}, socket) do
+    push socket, "player_joined", %{id: id}
+    {:noreply, socket}
+  end
+
+
 end
