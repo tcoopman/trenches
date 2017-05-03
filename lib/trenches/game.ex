7a8d04c313f5af0f39e7971d3832c9440cf48885
Thomas Coopman <thomas.coopman@gmail.com>
2017-05-03 22:22:40 +0200

    no more speed up after new unit

--- lib/trenches/game.ex
+++ lib/trenches/game.ex
@@ -43,7 +43,7 @@ defmodule Trenches.Game do
   end
 
   def init(state) do
-    state = %{unique_id: 0, players: %{}, subscribers: MapSet.new}
+    state = %{unique_id: 0, players: %{}, subscriber: nil}
     schedule_work()
     {:ok, state}
   end
@@ -72,9 +72,8 @@ defmodule Trenches.Game do
     {:reply, id, state}
   end
 
-  def handle_call({:subscribe, subscriber}, _from, %{subscribers: subscribers} = state) do
-    subscribers = MapSet.put(subscribers, subscriber)
-    state = %{state | subscribers: subscribers}
+  def handle_call({:subscribe, subscriber}, _from, state) do
+    state = %{state | subscriber: subscriber}
     {:reply, state, state}
   end
 
@@ -84,15 +83,14 @@ defmodule Trenches.Game do
       player = Player.add_unit(player, unit)
     end)
     state = %{state | players: players}
-    send(self(), :tick)
     {:reply, players, state}
   end
 
-  def handle_info(:tick, %{subscribers: subscribers, players: players} = state) do
+  def handle_info(:tick, %{subscriber: subscriber, players: players} = state) do
     players = tick(players)
-    Enum.each(subscribers, fn subscriber -> 
+    if subscriber != nil do
       send(subscriber, {:tick, players})
-    end)
+    end
     state = %{state | players: players}
     schedule_work()
     {:noreply, state}
