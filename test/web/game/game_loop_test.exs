8055c04b5b51886dbf5bdc1bf320f3d0a34dc976
Thomas Coopman <thomas.coopman@gmail.com>
2017-05-16 15:31:42 +0200

    refactor

--- test/web/game/game_loop_test.exs
+++ test/web/game/game_loop_test.exs
@@ -20,17 +20,14 @@ defmodule Trenches.GameLoopTest do
     assert game == GameLoop.tick(game)
   end
 
-  test "2 units collide with same strength, because nobody wins, no money is awarded", %{game: game} do
+  test "2 units collide with same strength, both units have 0 strength left", %{game: game} do
     unit1 = %Unit{type: :foo, position: 49, strength: 100, cost: 0, speed: 1}
     unit2 = %Unit{type: :foo, position: 49, strength: 100, cost: 0, speed: 1}
-    players = game.players
-    players = Map.update!(players, 1, fn player -> 
-      %{player | units: [unit1]}
-    end)
-    players = Map.update!(players, 2, fn player -> 
-      %{player | units: [unit2]}
-    end)
-    new_game = GameLoop.tick(%{game | players: players})
+
+    new_game = game
+    |> add_unit_to_player(1, unit1)
+    |> add_unit_to_player(2, unit2)
+    |> GameLoop.tick
 
     strength1 = new_game.players[1].units |> Enum.at(0) |> Map.get(:strength)
     strength2 = new_game.players[2].units |> Enum.at(0) |> Map.get(:strength)
@@ -38,7 +35,19 @@ defmodule Trenches.GameLoopTest do
     assert 0 = strength2
   end
 
-  test "stronger unit collides, money is awarded" do
+  test "stronger unit collides with weaker unit, the stronger unit wins", %{game: game} do
+    unit1 = %Unit{type: :foo, position: 49, strength: 120, cost: 0, speed: 1}
+    unit2 = %Unit{type: :foo, position: 49, strength: 100, cost: 0, speed: 1}
+    
+    new_game = game
+    |> add_unit_to_player(1, unit1)
+    |> add_unit_to_player(2, unit2)
+    |> GameLoop.tick
+
+    strength1 = new_game.players[1].units |> Enum.at(0) |> Map.get(:strength)
+    strength2 = new_game.players[2].units |> Enum.at(0) |> Map.get(:strength)
+    assert 20 = strength1
+    assert 0 = strength2
 
   end
 
@@ -48,5 +57,12 @@ defmodule Trenches.GameLoopTest do
 
   test "more then each one unit" do
     
+  end
+
+  defp add_unit_to_player(%Game{} = game, player_id, unit) do
+    players = Map.update!(game.players, player_id, fn player -> 
+      %{player | units: [unit | player.units]}
+    end)
+    %{game | players: players}
   end
 end
