6a836f9148a1a5e8b8ba0eeab6355f59102e5362
Michel Grootjans <michel.grootjans@gmail.com>
2017-05-16 22:53:57 +0200

    refactored Player tests

--- test/player/player_test.exs
+++ test/player/player_test.exs
@@ -8,32 +8,30 @@ defmodule Trenches.PlayerTest do
     [player: Player.new(1, 'player 1'), soldier: Unit.new("soldier")]
   end
 
-  test "verify the constructor", %{player: player} do
+  test "player starts with 1000 credits and no units", %{player: player} do
     assert player.id == 1
     assert player.name == 'player 1'
     assert player.units == []
     assert player.money == 1000
   end
 
-  test "add known unit generates a new player", %{player: player} do
+  test "buying exact amount", %{player: player} do
+    player = %{player | money: 300}
     player = Player.add_unit(player, "soldier")
+
     assert player.units == [Unit.new("soldier")]
-    assert player.money == 1000 - Unit.new("soldier").cost
+    assert player.money == 0
   end
 
-  test "add unknown unit doesnt change the player", %{player: player} do
+  test "going over budget fails", %{player: player} do
+    player = %{player | money: 299}
+    player = Player.add_unit(player, "soldier")
+
+    assert player.units == []
+    assert player.money == 299
+  end
+
+  test "add unknown unit", %{player: player} do
     assert Player.add_unit(player, "unknown") == player
-  end
-
-  test "going over budget fails", %{player: player, soldier: soldier} do
-    player = Enum.reduce(1..4, player, fn(_index, acc) -> Player.add_unit(acc, "soldier") end)
-
-    assert player.units == [soldier, soldier, soldier]
-    assert player.money == 1000 - 3*soldier.cost
-  end
-
-  test "buying exact amount", %{player: player, soldier: soldier} do
-    player = %{player | money: soldier.cost}
-    assert Player.add_unit(player, "soldier").money == 0
   end
 end
