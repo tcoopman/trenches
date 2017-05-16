771406452ff2bf1049b809b7b9ca11bb63714706
Michel Grootjans <michel.grootjans@gmail.com>
2017-05-16 15:50:49 +0200

    add unit

--- test/player/player_test.exs
+++ test/player/player_test.exs
@@ -2,12 +2,27 @@ defmodule Trenches.PlayerTest do
   use ExUnit.Case, async: true
 
   alias Trenches.Player
+  alias Trenches.Unit
 
-  test "verify the constructor" do
-    player = Player.new(1, 'player 1')
+  setup do
+    [player: Player.new(1, 'player 1')]
+  end
+
+  test "verify the constructor", %{player: player} do
     assert player.id == 1
     assert player.name == 'player 1'
     assert player.units == []
     assert player.money == 1000
   end
+
+  test "add known unit generates a new player", %{player: player} do
+    player = Player.add_unit(player, "soldier")
+    assert player.units == [Unit.new("soldier")]
+    assert player.money == 1000 - Unit.new("soldier").cost
+  end
+
+  test "add unknown unit doesnt change the player", %{player: player} do
+    assert Player.add_unit(player, "unknown") == player
+  end
+
 end
