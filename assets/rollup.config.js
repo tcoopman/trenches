304ab39cc6f12dad3b5eb819ff42bfc0885588f5
Thomas Coopman <thomas.coopman@gmail.com>
2017-06-04 20:59:03 +0200

    rollup for phoenix

--- assets/rollup.config.js
+++ assets/rollup.config.js
@@ -1,9 +1,18 @@
 import node_resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
     entry: './lib/es6/bs/main.js',
     format: 'cjs',
     dest: './js/main-bundled.js',
-    plugins: [node_resolve({module: true, browser: true})],
+    plugins: [
+        node_resolve({module: true, browser: true}),
+        commonjs({
+            include: 'node_modules/phoenix/**',
+            namedExports: {
+                'node_modules/phoenix/priv/static/phoenix.js': ['Socket']
+            }
+        })
+    ],
     moduleName: 'trenches'
 }
